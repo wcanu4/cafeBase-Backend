3c8e687fe6ca6c0f123dab9d273dda8e0495fac7
wcanu4 <112247343+wcanu4@users.noreply.github.com>
2023-11-14 15:11:30 -0600

    actualizando

--- config/config.js
+++ config/config.js
@@ -1,13 +1,29 @@
-// config.js
-const Sequelize = require('sequelize');
+// config/sequelize.js
 
-const sequelize = new Sequelize('cafebase', 'waltercanu', '123456789', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306,
-});
+const { Sequelize } = require('sequelize');
+const dotenv =require ('dotenv')
+dotenv.config()
+
+const dbCredencials ={
+  database:process.env.DB_NAME,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  host:process.env.DB_HOST,
+  port:process.env.DB_PORT,
+};
+
+
+const sequelize = new Sequelize(
+dbCredencials.database,
+dbCredencials.user,
+dbCredencials.password,
+{
+  port:dbCredencials.port,
+  host:dbCredencials.host,
+  dialect: "mysql",
+}
+);
+
 
 module.exports = sequelize;
-
-
 
